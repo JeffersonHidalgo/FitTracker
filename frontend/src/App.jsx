1d8c89ee2c997031a6ef540ff516d51859200c64
JeffersonHidalgo <jeffersonhidalgo853@gmai.com>
2025-02-18 17:11:44 -0400

    feat: add new branch Eren

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,18 @@
 
 import './App.css'
+import Carou from "./components/Carou";
 import Form from "./components/form";
 import Header from "./components/Header";
+import Plan from "./components/Plan";
 
 function App() {
   return (
     <>
       {" "}
       <Header />
+      <br />
+      <Plan />
+      <Form />
     </>
   );
 }

765a358ad5b36ecff60102c380080a2d8581a80d
Charlie Delgado <charliedelgado12@icloud.com>
2025-03-04 23:14:11 -0400

    Se agrego pantalla de usuario y pantalla de clientes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,26 @@
-
-import './App.css'
-import Carou from "./components/Carou";
-import Form from "./components/form";
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from "./components/Header";
 import Plan from "./components/Plan";
 import Charts from "./components/Charts";
+import Form from "./components/form";
+import UserPermissions from './components/UserPermissions';
+import Home from "./components/Home"; // Crear este componente con el contenido actual de la página principal
+import ClientForm from './components/clientForm';
 
 function App() {
   return (
-    <>
-      {" "}
+    <Router>
       <Header />
-      <br />
-      <Plan />
-      <br />
-      <br />
-      <br />
-      <br />
-      <Charts />
-      <p></p>
-      <Form />
-    </>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/planes" element={<Plan />} />
+        <Route path="/estadisticas" element={<Charts />} />
+        <Route path="/contacto" element={<Form />} />
+        <Route path="/gestion-usuarios" element={<UserPermissions />} />
+        <Route path="/registro/cliente" element={<ClientForm />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ frontend/src/components/Home.jsx
@@ -0,0 +1,18 @@
+// components/Home.jsx
+import Carou from "./Carou";
+import Plan from "./Plan";
+import Charts from "./Charts";
+import Form from "./form";
+
+const Home = () => {
+  return (
+    <>
+      <Carou />
+      <Plan />
+      <Charts />
+      <Form />
+    </>
+  );
+};
+
+export default Home;
